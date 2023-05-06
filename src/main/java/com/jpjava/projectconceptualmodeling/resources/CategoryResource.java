2cf84744d2b4be05ddbdf43db8a77cfabf080200
joaopedromendesjava <joaopedromendessilva@gmail.com>
2023-05-06 13:42:32 -0300

    Testing class domain category

--- src/main/java/com/jpjava/projectconceptualmodeling/resources/CategoryResource.java
+++ src/main/java/com/jpjava/projectconceptualmodeling/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.jpjava.projectconceptualmodeling.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jpjava.projectconceptualmodeling.model.Categories;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping()
-	public String list() {
-		return "Rest está funcionando";
+	public List<Categories> list() {
+		
+		Categories cat1 = new Categories((long) 1, "Informática");
+		Categories cat2 = new Categories((long) 2, "Escritório");
+		
+		List<Categories> listCategories = new ArrayList<>();
+		listCategories.add(cat1);
+		listCategories.add(cat2);
+		
+		return listCategories;
 	}
-
 }
+
