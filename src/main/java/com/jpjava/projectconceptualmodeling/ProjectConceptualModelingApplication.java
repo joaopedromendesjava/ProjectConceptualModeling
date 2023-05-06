4585d3d8a86645519d00642ac53962e39a0bc906
joaopedromendesjava <joaopedromendessilva@gmail.com>
2023-05-06 11:53:18 -0300

    Projeto Criado

---
+++ src/main/java/com/jpjava/projectconceptualmodeling/ProjectConceptualModelingApplication.java
@@ -0,0 +1,13 @@
+package com.jpjava.projectconceptualmodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectConceptualModelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectConceptualModelingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpjava/projectconceptualmodeling/ProjectConceptualModelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpjava.projectconceptualmodeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectConceptualModelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
