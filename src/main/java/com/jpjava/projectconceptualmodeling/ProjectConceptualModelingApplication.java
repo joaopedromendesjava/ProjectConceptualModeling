f4d734c3b252532bc28c004387818c0531d66737
joaopedromendesjava <joaopedromendessilva@gmail.com>
2023-05-06 15:36:26 -0300

    Create operation instance category

--- src/main/java/com/jpjava/projectconceptualmodeling/ProjectConceptualModelingApplication.java
+++ src/main/java/com/jpjava/projectconceptualmodeling/ProjectConceptualModelingApplication.java
@@ -1,13 +1,34 @@
 package com.jpjava.projectconceptualmodeling;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjectConceptualModelingApplication {
+import com.jpjava.projectconceptualmodeling.model.Category;
+import com.jpjava.projectconceptualmodeling.repository.CategoryRepository;
 
+@SpringBootApplication
+public class ProjectConceptualModelingApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectConceptualModelingApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception { //metodo implementado 
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+
 }
+
